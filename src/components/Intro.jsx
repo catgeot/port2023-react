ebcf7c0e9f205709132dbedfeb0679d4d8fe16ca
catgeot <catgeot@naver.com>
2023-10-17 08:40:34 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
 
+import about from "../assets/img/임시프로필.jpg";
+
+const introText = {
+  title: "port developer",
+  desc: ["talenr is", "found at the end of the", "effort"]
+}
+
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <section id="intro">
+            <div className="intro_inner">
+                <h2 className="intro_title">
+                    {introText.title}
+                </h2>
+               <div className="intro_lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+               </div>
+               <div className="intro_text">
+                    <div className="text">
+                    <div>{introText.desc[0]}</div>
+                    <div>{introText.desc[1]}</div>
+                    <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="about me" />
+                    </div>
+               </div> 
+               <div className="intro_lines bottom" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+           </div>
+            </div>
+        </section>
   )
 }
 
